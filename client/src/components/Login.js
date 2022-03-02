9489abac03ca8e668e4d07c581df2473e74135d6
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-03-02 13:00:45 -0500

    fixed login func

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,45 +1,93 @@
-import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Button } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Button, Alert, AlertIcon } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { setCurrentUser } from '../redux/reducers/userReducer';
 import Password from './Password'
 
 export default function ErrorMessageExample() {
     const [input, setInput] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
     const handleInputChange = (e) => setInput(e.target.value)
     const handlePasswordChange = (e) => setPassword(e.target.value)
     const isError = input === ''
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
 
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('/auth/local/login', {
+            method: 'POST',
+            redirect: 'follow',
+            body: JSON.stringify({
+                username: input,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.redirected) {
+                    return res.json()
+                }
+                return { success: 'successfully logged in' }
+            })
+            .then(data => {
+                if (data.error) {
+                    setError(data.error)
+                } else {
+                    console.log(data)
+                    setInput('')
+                    setPassword('')
+                    setError('')
+                    dispatch(setCurrentUser(data))
+                    navigate('/')
+                }
+            })
+    }
+    const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
     return (
-        <FormControl isInvalid={isError}>
-            <FormLabel htmlFor='username'>Username</FormLabel>
-            <Input
-                id='username'
-                type='text'
-                value={input}
-                onChange={handleInputChange}
-            />
-            {!isError ? (
-                <FormHelperText>
-                    Enter the username registered with kackle.
-                </FormHelperText>
-            ) : (
-                <FormErrorMessage>Password is required.</FormErrorMessage>
-            )}
-            <Password value={password}
-                onChange={handlePasswordChange} />
+        <form onSubmit={handleSubmit}>
+            <FormControl isInvalid={isError}>
+                {error && (
+                    <Alert maxWidth="400px" mx="auto" my="5" status='error' variant='subtle'>
+                        <AlertIcon />
+                        Error!&nbsp;{error}
+                    </Alert>
+                )}
+                <FormLabel htmlFor='username'>Username</FormLabel>
+                <Input
+                    id='username'
+                    type='text'
+                    value={input}
+                    name='username'
+                    onChange={handleInputChange}
+                />
+                {!isError ? (
+                    <FormHelperText>
+                        Enter the username registered with kackle.
+                    </FormHelperText>
+                ) : (
+                    <FormErrorMessage>Password is required.</FormErrorMessage>
+                )}
+                <Password value={password}
+                    onChange={handlePasswordChange} name='password' />
+                <Link to="../register">Dont have an account yet? Register Here.</Link>
 
-            <Button colorScheme='orange' variant='outline'>
-                Submit
-            </Button>
-            <Button as='a' href='http://localhost:3000/auth/facebook' colorScheme='facebook'>
-                Login with Facebook.
-            </Button>
-            <Button as='a' href='http://localhost:3000/auth/twitter' colorScheme='twitter'>
-                Login with twitter.
-            </Button>
-        </FormControl>
+                <Button type='submit' colorScheme='orange' variant='outline'>
+                    Submit
+                </Button>
+                <Button as='a' href={host + '/auth/facebook'} colorScheme='facebook'>
+                    Login with Facebook.
+                </Button>
+                <Button as='a' href={host + '/auth/twitter'} colorScheme='twitter'>
+                    Login with twitter.
+                </Button>
+            </FormControl>
+        </form>
     )
 }
 
--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,6 +1,5 @@
 const defaultState = {
-    username: null,
-    userId: null
+    user: null
 }
 
 const USER_SET_CURRENT = "USER_SET_CURRENT"
@@ -8,31 +7,26 @@ const USER_SET_CURRENT = "USER_SET_CURRENT"
 export function setCurrentUser(userObject) {
     return {
         type: USER_SET_CURRENT,
-        username: userObject.username,
-        userId: userObject.id
+        user: userObject
     }
 }
 
 export function fetchCurrentUser(dispatch, getState) {
-    const state = getState()
-
-    if(!state.user.username){
-        return
-    }
     fetch('/users/api/current')
-    .then(res => res.json())
-    .then(data => {
-        dispatch(setCurrentUser(data))})
+        .then(res => res.json())
+        .then(data => {
+            dispatch(setCurrentUser(data))
+        })
 }
 
-export default function userReducer(state=defaultState, action){
-    switch(action.type){
+export default function userReducer(state = defaultState, action) {
+    switch (action.type) {
         case USER_SET_CURRENT:
-            return { ...state,
-                username: action.username,
-                userId: action.userId
+            return {
+                ...state,
+                user: action.user,
             }
-        default: 
+        default:
             return state;
     }
 }
