a29bf0d2ebb5bbbfa76c41c820486edd01ab4671
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-03-03 11:36:58 -0500

    added friends list to socials pg

---
+++ client/src/components/ListOfFriends.js
@@ -0,0 +1,53 @@
+
+import React from 'react'
+import img from '../img/kacklelogo.png'
+import '../css/Friend.css'
+import { Heading } from '@chakra-ui/react'
+
+function ListOfFriends() {
+    return (
+        <div>
+            <Heading>Friends' List</Heading>
+            <ul id='friend-list'>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend One
+                    </div>
+                </li>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend Two
+                    </div>
+                </li>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend Three
+                    </div>
+                </li>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend Four
+                    </div>
+                </li>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend Five
+                    </div>
+                </li>
+                <li class='friend'>
+                    <img src={img} />
+                    <div class='name'>
+                        Friend Six
+                    </div>
+                </li>
+            </ul>
+        </div >
+    )
+}
+
+export default ListOfFriends
