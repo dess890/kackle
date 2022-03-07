bc36b0d908728ead88776bdc114924e566932fef
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-03-07 17:00:40 -0500

    deleted cards, added fbposts component

---
+++ client/src/components/FbPosts.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+function FbPosts(props) {
+    const { posts } = props;
+    return (
+        <div>FbPosts</div>
+    )
+}
+
+export default FbPosts
--- client/src/components/ScrollDiv.js
+++ client/src/components/ScrollDiv.js
@@ -1,31 +1,17 @@
 import "./ScrollDiv.css";
 import { BsFacebook } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 export default function ScrollDiv() {
+  const posts = useSelector((state) => state.FB.data)
   return (
     <div class="scroll">
       <h1>
         <BsFacebook />
+        {posts.map(post => {
+          return <TodoItem post={post} />
       </h1>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+
     </div>
   );
 }
